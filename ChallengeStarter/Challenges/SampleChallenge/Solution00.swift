a66d82523bed1d28fe6f9f267956a6b328439cd9
Radamés <r.alfaro8@icloud.com>
2022-11-30 22:06:30 -0400

    Renaming rawInput/rawOutput to improve readability

--- ChallengeStarter/Challenges/SampleChallenge/Solution00.swift
+++ ChallengeStarter/Challenges/SampleChallenge/Solution00.swift
@@ -31,10 +31,10 @@ extension SampleChallenge {
         
         // MARK: - Solution Methods
         // Step 1: Assemble
-        func assemble(_ input: String, _ output: String? = nil) -> (Input, Output?) {
-            let depths = input.integerList()
+        func assemble(_ rawInput: String, _ rawOutput: String? = nil) -> (Input, Output?) {
+            let depths = rawInput.integerList()
             
-            let increases = output?.integerList()[0]
+            let increases = rawOutput?.integerList()[0]
             
             return (depths, increases)
         }
